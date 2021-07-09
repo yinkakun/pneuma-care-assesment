149fa644e2954ffdd6443bf54b25b2ca499a6344
Yinka Adedire <yinkakun@gmail.com>
2021-07-09 11:42:04 +0100

    Setup Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+import HomePage from './homepage';
+import Character from './character';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/character/:character" component={Character} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
